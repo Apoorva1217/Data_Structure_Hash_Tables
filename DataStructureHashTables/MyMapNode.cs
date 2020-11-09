34941a2a1764e2f13bd2a3c0a7cb9bf431128092
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-09 19:10:42 +0530

    [Apoorva] Add . Ability to find frequency of words in a sentence like To be or not to be

---
+++ DataStructureHashTables/MyMapNode.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureHashTables
+{
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        /// <summary>
+        /// constructor of MyMapNode class
+        /// </summary>
+        /// <param name="size"></param>
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+
+        /// <summary>
+        /// Get the position of array elements
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        /// <summary>
+        /// Get the value by providing key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        /// <summary>
+        /// Add items in hash table
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+
+        /// <summary>
+        /// Get linkedlist
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+
+        /// <summary>
+        /// Get frequency of value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public void GetFrequency(V value)
+        {
+            int frequency = 0;
+            foreach (LinkedList<KeyValue<K, V>> list in items)
+            {
+                if (list == null)
+                {
+                    continue;
+                }
+                foreach (KeyValue<K, V> obj in list)
+                {
+                    if (obj.Equals(null))
+                    {
+                        continue;
+                    }
+                    if (obj.Value.Equals(value))
+                    {
+                        frequency++;
+                    }
+                }
+            }
+            Console.WriteLine("Frequency of {0} is {1}", value, frequency);
+        }
+    }
+}
--- DataStructureHashTables/Program.cs
+++ DataStructureHashTables/Program.cs
@@ -7,6 +7,20 @@ namespace DataStructureHashTables
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Hash Tables!");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value:" + hash5);
+            hash.GetFrequency("be");
+
         }
     }
 }
